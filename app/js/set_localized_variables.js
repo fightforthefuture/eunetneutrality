3af9228d4da96e502d6fd485fb0e9583336dd643
Jeff Lyon <jeff@rubbingalcoholic.com>
2016-06-24 18:45:00 -0500

    new license fixes for librejs

--- app/js/set_localized_variables.js
+++ app/js/set_localized_variables.js
@@ -1,6 +1,34 @@
 ---
 ---
-// @license magnet:?xt=urn:btih:5305d91886084f776adcf57509a648432709a7c7&dn=x11.txt X11
+/**
+ *
+ * @source: https://github.com/fightforthefuture/eunetneutrality
+ *
+ * @licstart  The following is the entire license notice for the
+ *  JavaScript code in this page.
+ *
+ * Copyright (C) Fight for the Future
+ *
+ *
+ * The JavaScript code in this page is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU
+ * General Public License (GNU GPL) as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version.  The code is distributed WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
+ *
+ * As additional permission under GNU GPL version 3 section 7, you
+ * may distribute non-source (e.g., minimized or compacted) forms of
+ * that code without the copy of the GNU GPL normally required by
+ * section 4, provided you include this license notice and a URL
+ * through which recipients can access the Corresponding Source.
+ *
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this page.
+ *
+ */
+
 var language  = (window.navigator.userLanguage || window.navigator.language).toLowerCase(),
     iso       = language.substr(0, 2);
 
@@ -25,6 +53,3 @@ var fullPageTranslations = [
 
 if (fullPageTranslations.indexOf(iso) !== -1 && iso != 'en' && window.location.pathname == '/')
   window.location.replace('/' + iso + window.location.search);
-
-
-// @license-end
