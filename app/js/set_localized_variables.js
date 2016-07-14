67a5961df2ec247852765dde60d74a250c646498
Jeff Lyon <jeff@rubbingalcoholic.com>
2016-07-14 13:34:32 -0500

    override locale index for headline translation

--- app/js/set_localized_variables.js
+++ app/js/set_localized_variables.js
@@ -34,6 +34,14 @@ var language  = (window.navigator.userLanguage || window.navigator.language).toL
 
 window.l10n || (window.l10n = {});
 
+var fullPageTranslations = [
+  {% for locale in site.data.l10n %}{% if locale.page_translation %}'{{ locale.code }}',{% endif %}{% endfor %}
+];
+
+var overrideLocaleIndex = fullPageTranslations.indexOf(window.location.pathname.replace(/\//g, ''));
+if (overrideLocaleIndex !== -1)
+    iso = fullPageTranslations[overrideLocaleIndex];
+
 switch (iso) {
   {% for locale in site.data.l10n %}
     case '{{ locale.code }}':
@@ -47,9 +55,6 @@ switch (iso) {
       {% endfor %}
 }
 
-var fullPageTranslations = [
-  {% for locale in site.data.l10n %}{% if locale.page_translation %}'{{ locale.code }}',{% endif %}{% endfor %}
-];
-
 if (fullPageTranslations.indexOf(iso) !== -1 && iso != 'en' && window.location.pathname == '/')
   window.location.replace('/' + iso + window.location.search);
+
